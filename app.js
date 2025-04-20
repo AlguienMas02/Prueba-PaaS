569df9030626fffc1951bbbd5357dabe49cabe9f
Alguien <mpaulinodurand2015@gmail.com>
2025-04-19 19:33:49 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 3000;
+
+app.get('/', (req, res)=>{
+    req.setEncoding('Despliege paas 2025');
+});
+
+app.listen(port,()=>{
+    console.log('Aplicacion escuchando en el puerto $(port)')
+
+
+});
+
+
+
+
+
+
+
+
